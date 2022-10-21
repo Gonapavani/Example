280737beb71af4060060340db996acebb269f3b5
Gonapavani <116263874+Gonapavani@users.noreply.github.com>
2022-10-21 10:55:24 +0530

    Add files via upload

---
+++ evennumbers.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class evennumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=50,i;
+		for(i=10;i<number;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i+ " ");
+			}
+		}
+
+	}
+
+}
